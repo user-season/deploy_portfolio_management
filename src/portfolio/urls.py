981569e3f038c8c9b8a19498d30b62cec4f29b1d
Dao Tien Sang <tiensangdao2004@gmail.com>
2025-05-01 16:20:10 +0700

    add transaction template

--- src/portfolio/urls.py
+++ src/portfolio/urls.py
@@ -7,5 +7,12 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
+
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+
+    path('assets/', views.asset_list, name='asset_list'),
+    path('portfolios/', views.portfolio_list, name='portfolio_list'),
+    path('transactions/', views.transaction_list, name='transaction_list'),
     
 ]
--- src/portfolio/views.py
+++ src/portfolio/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 
-def dashboard(request):
-    return render(request, 'portfolio/dashboard.html')
 
 def home(request):
     return render(request, 'portfolio/home.html')
 
+def dashboard(request):
+    return render(request, 'portfolio/dashboard.html')
+
+def login_view(request):
+    return render(request, 'portfolio/login.html')
+
+def register(request):
+    return render(request, 'portfolio/register.html')
+
+def portfolio_list(request):
+    return render(request, 'portfolio/portfolio_list.html')
+
+def asset_list(request):
+    return render(request, 'portfolio/asset_list.html')
+
+def transaction_list(request):
+    return render(request, 'portfolio/transaction_list.html')
