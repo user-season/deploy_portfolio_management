ed9772cb8038abfc90c4752786a73907a4a885aa
Nguyễn Chí Trung <trungtada123@gmail.com>
2025-05-01 15:19:48 +0700

    Start Webapp

--- src/portfolio/apps.py
+++ src/portfolio/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UserloginConfig(AppConfig):
+class PortfolioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'userLogin'
+    name = 'portfolio'
