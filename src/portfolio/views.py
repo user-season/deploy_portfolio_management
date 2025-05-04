ec68c3edb5021887de25317dc95414ceefcf9d28
DataVuong <tiger.data105@gmail.com>
2025-05-04 16:03:19 +0700

    add sell stock template

--- src/portfolio/views.py
+++ src/portfolio/views.py
@@ -25,11 +25,25 @@ def portfolio_create(request):
 def asset_list(request):
     return render(request, 'portfolio/asset_list.html')
 
+
 def transaction_list(request):
     return render(request, 'portfolio/transaction_list.html')
 
+def transaction_create(request):
+    return render(request, 'portfolio/transaction_form.html')
+
 def wallet(request):
     return render(request, 'portfolio/wallet.html')
+
+def wallet_transactions(request):
+    return render(request, 'portfolio/wallet_transactions.html')
+    
+def bank_account_list(request):
+    return render(request, 'portfolio/bank_account_list.html')
+
+def bank_account_create(request):
+    return render(request, 'portfolio/bank_account_form.html')
+    
 
 def deposit_money(request):
     return render(request, 'portfolio/deposit.html')
